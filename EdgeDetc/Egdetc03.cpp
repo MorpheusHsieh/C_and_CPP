264a5d914c14008586cd60b0f91c9bc756635151
Morpheus9631 <morpheus.hsieh@gmail.com>
2018-04-09 00:04:35 +0800

    Add files via upload

---
+++ EdgeDetc/Egdetc03.cpp
@@ -0,0 +1,45 @@
+// Program : EdgeDect.CPP
+// Author  : Avatar
+// Date    : 98.11.25
+#include "ImgStrut.h"
+#include <math.h>
+#include <stdlib.h>
+
+void Edge_Detection(Image*);
+extern int Input_Image_Info(Image*, int);
+extern void Laplician(Image*);
+
+void main(void)
+{
+  clrscr();
+  printf("Image edge detection.\n");
+
+  Image img1, img2;
+  if (!Input_Image_Info(&img1,1)) exit(1);
+  if (!Input_Image_Info(&img2,2)) exit(1);
+
+  int i, j, width = img2.Width, height = img2.Height;
+  for (i=0; i<height; i++)
+    for (j=0; j<width; j++)
+      img2.pixel[i*width+j] = img1.pixel[i*width+j];
+
+  Laplician(&img2);
+
+  fwrite(img2.pixel,height,width,img2.fptr);
+  fclose(img2.fptr);
+
+  printf("\nPress ane key to conitune...");
+  getch();
+}
+
+
+
+
+
+
+
+
+
+
+
+
